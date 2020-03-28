29d38e5e2ea95bfe761d8039a32a7313c5d93f16
rajitest3 <62781495+rajitest3@users.noreply.github.com>
2020-03-27 20:05:18 -0700

    Update countVisibleNodes.java

--- countVisibleNodes.java
+++ countVisibleNodes.java
@@ -2,6 +2,26 @@
 In a binary tree, if in the path from root to the node A, 
 there is no node with greater value than A’s, 
 this node A is visible. We need to count the number of visible nodes in a binary tree.
+
+Input:
+        5
+     /     \
+   3        10
+  /  \     /
+20   21   1
+
+Output: 4
+Explanation: There are 4 visible nodes: 5, 20, 21, and 10.
+
+Input:
+  -10
+	\
+	-15
+	   \
+	   -1
+
+Output: 2
+Explanation: Visible nodes are -10 and -1.
 */
 
 // Time: O(n)
